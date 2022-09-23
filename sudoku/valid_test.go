a1685c9c0f43b61d2ef9bce88a5ba9acd3b6b041
Austin Meyer <frizzkitten@gmail.com>
2022-09-23 18:45:23 -0500

    isValid tests

--- sudoku/valid_test.go
+++ sudoku/valid_test.go
@@ -21,4 +21,79 @@ func TestIsValid(t *testing.T) {
 
 		assert.True(t, s.IsValid())
 	})
+
+	t.Run("invalid row", func(t *testing.T) {
+		s := Sudoku{
+			scale:     4,
+			scaleRoot: 2,
+			Rows: []Row{
+				Row{1, 2, 2, 4},
+				Row{3, 4, 1, 3},
+				Row{2, 3, 4, 1},
+				Row{4, 1, 3, 2},
+			},
+		}
+
+		assert.False(t, s.IsValid())
+	})
+
+	t.Run("invalid column", func(t *testing.T) {
+		s := Sudoku{
+			scale:     4,
+			scaleRoot: 2,
+			Rows: []Row{
+				Row{1, 2, 3, 4},
+				Row{3, 4, 2, 1},
+				Row{3, 1, 4, 2},
+				Row{4, 2, 1, 3},
+			},
+		}
+
+		assert.False(t, s.IsValid())
+	})
+
+	t.Run("invalid box", func(t *testing.T) {
+		s := Sudoku{
+			scale:     4,
+			scaleRoot: 2,
+			Rows: []Row{
+				Row{1, 2, 3, 4},
+				Row{2, 4, 1, 3},
+				Row{3, 1, 4, 2},
+				Row{4, 3, 2, 1},
+			},
+		}
+
+		assert.False(t, s.IsValid())
+	})
+
+	t.Run("invalid number (too big)", func(t *testing.T) {
+		s := Sudoku{
+			scale:     4,
+			scaleRoot: 2,
+			Rows: []Row{
+				Row{1, 2, 3, 4},
+				Row{3, 4, 1, 2},
+				Row{2, 3, 5, 1},
+				Row{4, 1, 2, 3},
+			},
+		}
+
+		assert.False(t, s.IsValid())
+	})
+
+	t.Run("invalid number (too small)", func(t *testing.T) {
+		s := Sudoku{
+			scale:     4,
+			scaleRoot: 2,
+			Rows: []Row{
+				Row{1, 2, 3, 4},
+				Row{3, 4, 1, 0},
+				Row{2, 3, 4, 1},
+				Row{4, 1, 2, 3},
+			},
+		}
+
+		assert.False(t, s.IsValid())
+	})
 }
